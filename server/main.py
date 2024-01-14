dd3b1e534222a5df9c69d6e88498bec4d0bd9910
amakows1 <01168897@pw.edu.pl>
2024-01-14 23:50:23 +0100

    odczytanie danych z bazy zamiast z API poprzez localhost/currency/{waluta}

--- server/main.py
+++ server/main.py
@@ -3,9 +3,11 @@ from flask_pymongo import PyMongo
 import requests
 from flask_cors import cross_origin
 import ast
+from pymongo import MongoClient
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/nbp"
+client = MongoClient("localhost", 27017)
 mongo = PyMongo(app)
 
 @app.route("/")
@@ -22,7 +24,12 @@ def get_currency(currency):
     url=f"http://api.nbp.pl/api/exchangerates/rates/a/{currency}/"
     response = requests.get(url, headers=header).content.decode('UTF-8')
     todays_usd = ast.literal_eval(response)
-    return todays_usd
+    database = client['nbp']
+    tableA = database['tableA']
+    aa = tableA.find({"code": currency})
+    for i in aa:
+        print(i)
+    return str(i)
 
 @app.route("/currencies_names")
 @cross_origin()
@@ -39,6 +46,11 @@ def currencies_json():
         rates = ast.literal_eval(currencies_json)[0]['rates']
     except:
         rates = "Brak danych"
-    return rates
+    database = client['nbp']
+    tableA = database['tableA']
+    aa = tableA.find({"code": "USD"})
+    for i in aa:
+        print(i)
+    return str(i)
 
 app.run(debug=True)
